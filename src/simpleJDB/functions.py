ca246f966d075f2a4bee28c8f8d257ebad3c5b76
EightOhFour <73658510+Milkman337@users.noreply.github.com>
2023-01-15 18:28:12 +0100

    added commit feature

--- src/simpleJDB/functions.py
+++ src/simpleJDB/functions.py
@@ -23,14 +23,17 @@ class database:
         try:
             with open(f"{self.name}.json", "r") as f:
                 self.data = json.load(f)
+                self.staged_data = self.data.copy()
         except:
             with open(f"{self.name}.json", "w") as f:
                 f.write("{\"main\": []}")
             with open(f"{self.name}.json", "r") as f:
                 self.data = json.load(f)
+                self.staged_data = self.data.copy()
         self.lock = threading.Lock()
 
-    def setkey(self, keyname: str, value):
+
+    def setkey(self, keyname: str, value) -> None:
         """
         Add or update a keyname and its value in the json file.
 
@@ -44,19 +47,16 @@ class database:
         with self.lock:
             data_type = type(value).__name__
             gk = 0
-            for thing in self.data["main"]:
+            for thing in self.staged_data["main"]:
                 if thing["keyname"] == keyname:
                     thing["value"] = value
                     thing["datatype"] = data_type
                     gk = 1
-                    with open(f"{self.name}.json", "w") as f:
-                        json.dump(self.data, f)
             if gk == 0:
-                self.data["main"].append({"keyname": keyname, "value": value, "datatype": data_type})
-                with open(f"{self.name}.json", "w") as f:
-                    json.dump(self.data, f)
+                self.staged_data["main"].append({"keyname": keyname, "value": value, "datatype": data_type})
 
-    def delkey(self, keyname:str):
+
+    def delkey(self, keyname:str) -> None:
         """
         Delete a keyname and its value from the json file.
 
@@ -70,13 +70,12 @@ class database:
             TypeError: If the keyname is not found in the json file.
         """
         with self.lock:
-            for thing in self.data["main"]:
+            for thing in self.staged_data["main"]:
                 if thing["keyname"] == keyname:
-                    self.data["main"].remove(thing)
-                    with open(f"{self.name}.json", "w") as f:
-                        json.dump(self.data, f)
+                    self.staged_data["main"].remove(thing)
                     return
             raise TypeError("Key has not been found.")
+
 
     def gettype(self, keyname:str):
         """
@@ -118,4 +117,14 @@ class database:
             raise TypeError("Key has not been found.")
 
 
-    
+    def commit(self) -> None:
+        """
+        Writes the current state of the 'staged_data' attribute to the json file.
+        
+        Returns:
+            None
+        """
+        with self.lock:
+            self.data = self.staged_data.copy()
+            with open(f"{self.name}.json", "w") as f:
+                json.dump(self.data, f)
--- src/test_functions.py
+++ src/test_functions.py
@@ -12,25 +12,30 @@ class TestDatabase(unittest.TestCase):
 
     def test_setkey(self):
         self.db.setkey("key1", "value1")
+        self.db.commit()
         self.assertEqual(self.db.getkey("key1"), "value1")
         self.assertEqual(self.db.gettype("key1"), "str")
         self.db.setkey("key1", 1)
+        self.db.commit()
         self.assertEqual(self.db.getkey("key1"), 1)
         self.assertEqual(self.db.gettype("key1"), "int")
 
     def test_delkey(self):
         self.db.setkey("key1", "value1")
         self.db.delkey("key1")
+        self.db.commit()
         self.assertRaises(TypeError, self.db.getkey, "key1")
         self.assertRaises(TypeError, self.db.delkey, "key1")
 
     def test_getkey(self):
         self.db.setkey("key1", "value1")
+        self.db.commit()
         self.assertEqual(self.db.getkey("key1"), "value1")
         self.assertRaises(TypeError, self.db.getkey, "key2")
 
     def test_gettype(self):
         self.db.setkey("key1", "value1")
+        self.db.commit()
         self.assertEqual(self.db.gettype("key1"), "str")
         self.assertRaises(TypeError, self.db.gettype, "key2")
 
